c1a92db4fd0e03d33ce9d13ffdd01dbc26afcb2b
painter890602 <124643595+princepainter@users.noreply.github.com>
2026-01-25 21:09:11 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,18 @@
 from .PainterAI2V import PainterAI2V
 from .PainterAV2V import PainterAV2V
-from comfy_api.latest import ComfyExtension, io
-from typing import override
 
 __version__ = "1.0.0"
 
-class PainterExtension(ComfyExtension):
-    @override
-    async def get_node_list(self) -> list[type[io.ComfyNode]]:
-        return [PainterAI2V, PainterAV2V]
+NODE_CLASS_MAPPINGS = {
+    "PainterAI2V": PainterAI2V,
+    "PainterAV2V": PainterAV2V,
+}
 
-async def comfy_entrypoint():
-    return PainterExtension()
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "PainterAI2V": "Painter AI2V",
+    "PainterAV2V": "Painter AV2V",
+}
+
+WEB_DIRECTORY = "./web"
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
